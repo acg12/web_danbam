f218a1580677e78ac89c9abbe6211ff1cea8c33d
acg12 <56991979+acg12@users.noreply.github.com>
2021-04-08 00:16:06 +0700

    menu page

--- scripts/menu.js
+++ scripts/menu.js
@@ -59,3 +59,93 @@ $(document).ready(function() {
         }
     }
 })
+
+var lightbites = document.getElementById("menu-light");
+var maincourse = document.getElementById("menu-main");
+var drinks = document.getElementById("menu-drinks");
+var desserts = document.getElementById("menu-desserts");
+
+var allmenu = document.getElementById("categ-cont");
+var menu_child = allmenu.children;
+
+console.log(menu_child);
+
+lightbites.onclick = function(e) {
+    var lb_content = document.getElementById("light-bites");
+    if (lb_content.classList.contains("menu-non-active")) {
+        lb_content.classList.remove("menu-non-active");        
+    }
+    lb_content.classList.add("menu-active");
+
+    Array.prototype.forEach.call(menu_child, (item, i) => {
+        if (item != lb_content) {
+            if (item.classList.contains("menu-active")) {
+                item.classList.remove("menu-active");
+            }
+
+            if (!item.classList.contains("menu-non-active")) {
+                item.classList.add("menu-non-active");
+            }
+        }
+    })
+}
+
+maincourse.onclick = function(e) {
+    var mc_content = document.getElementById("main-course");
+    if (mc_content.classList.contains("menu-non-active")) {
+        mc_content.classList.remove("menu-non-active");        
+    }
+    mc_content.classList.add("menu-active");
+
+    Array.prototype.forEach.call(menu_child, (item, i) => {
+        if (item != mc_content) {
+            if (item.classList.contains("menu-active")) {
+                item.classList.remove("menu-active");
+            }
+
+            if (!item.classList.contains("menu-non-active")) {
+                item.classList.add("menu-non-active");
+            }
+        }
+    })
+}
+
+drinks.onclick = function(e) {
+    var dr_content = document.getElementById("drinks");
+    if (dr_content.classList.contains("menu-non-active")) {
+        dr_content.classList.remove("menu-non-active");        
+    }
+    dr_content.classList.add("menu-active");
+
+    Array.prototype.forEach.call(menu_child, (item, i) => {
+        if (item != dr_content) {
+            if (item.classList.contains("menu-active")) {
+                item.classList.remove("menu-active");
+            }
+
+            if (!item.classList.contains("menu-non-active")) {
+                item.classList.add("menu-non-active");
+            }
+        }
+    })
+}
+
+desserts.onclick = function(e) {
+    var de_content = document.getElementById("desserts");
+    if (de_content.classList.contains("menu-non-active")) {
+        de_content.classList.remove("menu-non-active");        
+    }
+    de_content.classList.add("menu-active");
+
+    Array.prototype.forEach.call(menu_child, (item, i) => {
+        if (item != de_content) {
+            if (item.classList.contains("menu-active")) {
+                item.classList.remove("menu-active");
+            }
+
+            if (!item.classList.contains("menu-non-active")) {
+                item.classList.add("menu-non-active");
+            }
+        }
+    })
+}
