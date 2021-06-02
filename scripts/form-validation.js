30fa250032d15eea11886d8aa9d8d80ff326ddf3
amelhcilab <amalliashrmn@gmail.com>
2021-06-02 20:46:26 +0700

    yeye

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -12,13 +12,19 @@ function validateForm() {
     var name = form["name"].value
     var email = form["email"].value
     var gender = form["gender"]
+    var branch = form["branch"]
+    var subject = form["subject"].value
 
     if(name.length == 0) {
-      alert("Name must be filled!")
+      alert("Name field must be filled!")
     } else if(email.length == 0) {
-      alert("Email must be filled!")
+      alert("Email field must be filled!")
     } else if(!gender[0].checked && !gender[1].checked && !gender[2].checked) {
       alert("Gender must be chosen!")
+    } else if(branch.value == "-1") {
+      alert( "Branch location must be chosen!")
+    } else if(subject.length == 0){
+      alert("Type your message!")
     } else {
       return true
     }
