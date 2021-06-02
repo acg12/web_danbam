349898784e762b75ca2c08a5bf841bd243acc763
amelhcilab <amalliashrmn@gmail.com>
2021-06-03 00:28:48 +0700

    fdgd

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -1,9 +1,4 @@
 function validateForm() {
-    // var x = document.forms["myForm"]["fname"].value;
-    // if (x == "") {
-    //   alert("Name must be filled out");
-    //   return false;
-    // } 
 
     var form = document.forms["contactForm"]
     var error = null
@@ -21,12 +16,13 @@ function validateForm() {
       alert("Email field must be filled!")
     } else if(!gender[0].checked && !gender[1].checked && !gender[2].checked) {
       alert("Gender must be chosen!")
-    } else if(branch.value == "-1") {
-      alert( "Branch location must be chosen!")
+    } else if(branch.selectedIndex == 0) {
+      alert("Branch location must be chosen!")
     } else if(subject.length == 0){
       alert("Type your message!")
     } else {
       return true
     }
+    
     return false
 }
