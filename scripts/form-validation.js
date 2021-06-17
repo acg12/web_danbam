e099e0338f7112f1675dd5c68d03691cf1f3f58a
acg12 <56991979+acg12@users.noreply.github.com>
2021-06-17 14:10:15 +0700

    beresin contact us + validation 5 types

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -1,26 +1,39 @@
 function validateForm() {
-
     var form = document.forms["contactForm"]
-    var error = null
-    var msg = null
     
     var name = form["name"].value
     var email = form["email"].value
-    var gender = form["gender"]
-    var branch = form["branch"]
-    var subject = form["subject"].value
+    var gender = form["gender"].value
+    var branch = form["branch"].value
+    var message = form["message"].value
+    var offer = form["offer"]
 
-    if(name.length == 0) {
-      alert("Name field must be filled!")
-    } else if(email.length == 0) {
-      alert("Email field must be filled!")
-    } else if(!gender[0].checked && !gender[1].checked && !gender[2].checked) {
-      alert("Gender must be chosen!")
-    } else if(branch.selectedIndex == 0) {
-      alert("Branch location must be chosen!")
-    } else if(subject.length == 0){
-      alert("Type your message!")
+    if(name == "" || email == "" || gender == "" || branch == "" || message == "") {
+      alert("All fields must be filled!")
+    } else if(email.search("@") == -1 || email.search(".com") == -1) {
+      alert("Email must be in valid format!")
+    } else if(!validName(name)) {
+      alert("Name must only consist of letters!")
+    } else if(!validMessage(message)) {
+      alert("Message must be longer than 5 characters!")
+    } else if(!offer.checked) {
+      alert("Please check the box for more exclusive details from DanBam!")
     } else {
-      alert("Success!")
+      alert("Thank you for contacting us! Our team will get back to you as soon as possible!")
+      document.getElementById("btn-reset").click();
     }
+}
+
+function validName(name) {
+  for(let i = 0; i < name.length; i++) {
+    var c = name.charAt(i);
+    if(!(c >= 'A' && c <= 'Z') && !(c >= 'a' && c <= 'z')) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function validMessage(msg) {
+  return (msg.length > 5) ? true : false;
 }
