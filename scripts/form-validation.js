26a835327a95c811c7fe8fae174ddc92aa2c0bf7
acg12 <56991979+acg12@users.noreply.github.com>
2021-06-19 09:24:36 +0700

    iscapital di name form

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -12,19 +12,19 @@ function validateForm() {
       alert("All fields must be filled!")
     } else if(email.search("@") == -1 || email.search(".com") == -1) {
       alert("Email must be in valid format!")
-    } else if(!validName(name)) {
+    } else if(!isAlpha(name)) {
       alert("Name must only consist of letters!")
     } else if(!validMessage(message)) {
       alert("Message must be longer than 5 characters!")
-    } else if(!offer.checked) {
-      alert("Please check the box for more exclusive details from DanBam!")
+    } else if(!firstCapital(name)) {
+      alert("Name must start with capital letter!")
     } else {
       alert("Thank you for contacting us! Our team will get back to you as soon as possible!")
       document.getElementById("btn-reset").click();
     }
 }
 
-function validName(name) {
+function isAlpha(name) {
   for(let i = 0; i < name.length; i++) {
     var c = name.charAt(i);
     if(!(c >= 'A' && c <= 'Z') && !(c >= 'a' && c <= 'z')) {
@@ -36,4 +36,12 @@ function validName(name) {
 
 function validMessage(msg) {
   return (msg.length > 5) ? true : false;
+}
+
+function firstCapital(name) {
+  var c = name.charAt(0);
+  if(!(c >= 'A' && c <= 'Z')) {
+    return false;
+  }
+  return true;
 }
